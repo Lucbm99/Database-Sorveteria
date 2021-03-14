48bf8cfb0f89b24eed37d18f1be6edfb7d09808c
Lucbm99 <45500959+Lucbm99@users.noreply.github.com>
2021-03-14 14:39:15 -0300

    Update dump_sorveteria.sql

--- dump_sorveteria.sql
+++ dump_sorveteria.sql
@@ -1,34 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.9.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Tempo de geração: 14-Out-2020 às 18:25
--- Versão do servidor: 10.4.10-MariaDB
--- versão do PHP: 7.2.25
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `sorveteria`
---
-
--- --------------------------------------------------------
+Create database sorveteria;
 
 --
 -- Estrutura da tabela `cliente`
 --
 
-DROP TABLE IF EXISTS `cliente`;
 CREATE TABLE IF NOT EXISTS `cliente` (
   `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) DEFAULT NULL,
@@ -54,7 +30,6 @@ INSERT INTO `cliente` (`id_cliente`, `nome`, `email`, `nascimento`) VALUES
 -- Estrutura da tabela `sorvete`
 --
 
-DROP TABLE IF EXISTS `sorvete`;
 CREATE TABLE IF NOT EXISTS `sorvete` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tipo` varchar(100) DEFAULT NULL,
@@ -75,9 +50,7 @@ INSERT INTO `sorvete` (`id`, `tipo`, `sabor`, `preco_unitario`, `id_cliente`) VA
 (3, 'sorv massa', 'céu azul', '2.30', 4),
 (4, 'sorv massa', 'chocolate', '2.40', 5),
 (5, 'picolé', 'flocos', '1.44', 2),
-(6, 'sorv massa', 'brigadeiro', '2.22', 3),
-COMMIT;
+(6, 'sorv massa', 'brigadeiro', '2.22', 3);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+select * from cliente;
+select * from sorvete;
